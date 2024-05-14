eb4861d69b1f52b16bc47d03d68e3b8ff64927fd
deepak <deepak.webperfection@gmail.com>
2024-05-14 13:06:10 +0530

    change the api

--- index.js
+++ index.js
@@ -1,93 +1,33 @@
-// import express from "express";
-// import axios from 'axios';
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Welcome to my server!');
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-
-
-// const express = require('express');
-// const axios = require('axios');
-
-// const app = express();
-// const port =  3000;
-
-// app.use(express.json());
-
-
-// app.get('/checkout', async (req, res) => {
-//     try {
-//         const response = await axios.get('https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/5a676375-9921-40c8-9970-6685d845b673',{
-//             headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-//                 'X-Auth-Token': 'gsg7tpt974p4yoiunecoh8r8oog84s9',
-//                 'Access-Control-Allow-Origin' : 'https://internationalpartnerseu.com',
-//                 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-//                 'Access-Control-Allow-Headers': 'Content-Type',
-//             },
-//             credentials: true
-//         });
-//         res.json(response.data);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
-// app.listen(port, () => {
-//     console.log(`Proxy server listening at http://localhost:${port}`);
-// });
-
-
-
-
-// In your custom server file (e.g., server.js)
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
+const app = express();
+const port =  3000;
+
+app.use(cors());
 
 
-  const server = express();
+app.get('/api/checkout-data', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/5a676375-9921-40c8-9970-6685d845b673',{
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'X-Auth-Token': 'gsg7tpt974p4yoiunecoh8r8oog84s9',
+                'Access-Control-Allow-Origin' : 'https://internationalpartnerseu.com',
+                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
-  // Enable CORS
-  server.use(cors());
+app.listen(port, () => {
+    console.log(`Proxy server listening at http://localhost:${port}`);
+});
 
-  // Your other server routes
-  // server.get('/api/someendpoint', (req, res) => {...});
-
-    server.get('/api/checkout', async (req, res) => {
-        try {
-            const response = await axios.get('https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/5a676375-9921-40c8-9970-6685d845b673',{
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                    'X-Auth-Token': 'gsg7tpt974p4yoiunecoh8r8oog84s9',
-                    'Access-Control-Allow-Origin' : '*',
-                    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-                    'Access-Control-Allow-Headers': 'Content-Type',
-                },
-                credentials: true
-            });
-            res.json(response.data);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Internal server error' });
-        }
-    });
-
-//   server.all('*', (req, res) => {
-//     return handle(req, res);
-//   });
-
-  server.listen(3000, err => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
 
