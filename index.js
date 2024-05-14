0c030d5301ac88b35e36f766c63e7b806e181008
deepak <deepak.webperfection@gmail.com>
2024-05-14 11:58:05 +0530

    change the api

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const port =  3000;
 
 // app.use(express.json());
 app.use(cors({
-    origin: 'https://internationalpartnerseu.com', // use your actual domain name (or localhost), using * is not recommended
+    origin: '*', // use your actual domain name (or localhost), using * is not recommended
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
