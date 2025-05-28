d9a375957a030c8efa13432db85ff227acc93450
terawebdeveloper <webdeveloper@terasystem.eu>
2025-05-28 16:21:43 +0530

    fixing internal error 505

--- index.js
+++ index.js
@@ -13,14 +13,21 @@ app.use(cors({
 
 app.get('/api/checkout-data', async (req, res) => {
     try {
-        const response = await axios.get('https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/5a676375-9921-40c8-9970-6685d845b673',{
+        // const response = await axios.get('https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/5a676375-9921-40c8-9970-6685d845b673',{
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        //         'X-Auth-Token': 'gsg7tpt974p4yoiunecoh8r8oog84s9',
+        //         'Access-Control-Allow-Origin' : 'https://internationalpartnerseu.com',
+        //         'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        //         'Access-Control-Allow-Headers': 'Content-Type',
+        //     }
+        // });
+
+        const response = await axios.get('https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/5a676375-9921-40c8-9970-6685d845b673', {
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 'X-Auth-Token': 'gsg7tpt974p4yoiunecoh8r8oog84s9',
-                'Access-Control-Allow-Origin' : 'https://internationalpartnerseu.com',
-                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type',
             }
         });
         console.log(`Response status: ${response}`);
