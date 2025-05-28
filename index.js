7068455c0bb89d54226cd3a38dee43519492f262
terawebdeveloper <rahulseefat@gmail.com>
2025-05-28 15:49:54 +0530

    fixing internal error 505

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+// const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const app = express();
-const port =  3000;
+// const app = express();
+// const port =  3000;
 
 // app.use(cors());
 app.use(cors({
@@ -30,8 +30,8 @@ app.get('/api/checkout-data', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Proxy server listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Proxy server listening at http://localhost:${port}`);
+// });
 
 
