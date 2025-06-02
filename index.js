8db80f50f7f8c8f4989284c60b13a8b47b915e80
terawebdeveloper <webdeveloper@terasystem.eu>
2025-06-02 09:56:39 +0530

    fixing handling amout

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
-const port = 3000;
- 
+const port =  3000;
+
+// app.use(cors());
+app.use(cors({
+    origin: 'https://internationalpartnerseu.com',
+    credentials: true
+}));
+
+
 app.get('/api/checkout-data', async (req, res) => {
-    const checkoutId = req.query.checkout_id;
+      const checkoutId = req.query.checkout_id;
  
     if (!checkoutId) {
         return res.status(400).json({ error: 'Missing checkout_id parameter' });
     }
- 
-    try {
-        const response = await axios.get(`https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/${checkoutId}`, {
+    try {                                                                                                                                   
+        const response = await axios.get(`https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/b9f238c9-ae3f-4a9f-b702-bb9df140e31c/${checkoutId}`,{
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 'X-Auth-Token': 'gsg7tpt974p4yoiunecoh8r8oog84s9',
-                'Access-Control-Allow-Origin': 'https://internationalpartnerseu.com',
+                'Access-Control-Allow-Origin' : 'https://internationalpartnerseu.com',
                 'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type',
             }
         });
- 
-        console.log(`Fetched data for checkout ID: ${checkoutId}`);
+
+        console.log(`Response status: ${response}`);
+        
         res.json(response.data);
     } catch (error) {
-        console.error('Error fetching checkout data:', error.message);
-        res.status(500).json({ error: 'Internal server error' });
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error xx' });
     }
 });
- 
+
 app.listen(port, () => {
-    console.log(`Proxy server listening at http://localhost:${port}`);
+    console.log(`Proxy server listening at xx http://localhost:${port}`);
 });
+
+
