bcc2009bf28e36cc3df610a55737dc7a23e5322c
terawebdeveloper <webdeveloper@terasystem.eu>
2025-06-05 18:50:18 +0530

    fixing handling checkous8

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 app.get('/api/checkout-data', async (req, res) => {
     try {                                                                                                                                   
-        const response = await axios.get('https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/b9f238c9-ae3f-4a9f-b702-bb9df140e31c',{
+        const response = await axios.get('https://api.bigcommerce.com/stores/d3h8howbsb/v3/checkouts/',{
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
